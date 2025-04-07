0787d29178ea772e37bae690a6ea3b77f76ebb23
Kundan <kundanss22@gmail.com>
2025-04-07 17:53:14 +0530

    add shadCN

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Kundan Suthar</h1>
+      <Button>ShadCN button</Button>
     </div>
   );
 }
